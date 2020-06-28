860153ee80c537fe6275ecbfb9959a39dec11c6a
Shrihan Dadi <dshrihan@gmail.com>
2020-06-28 11:59:21 -0400

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Tracker } from "./Tracker/Tracker";
 import { fetchData } from "./api";
+import styles from "./App.module.css";
 
 class App extends React.Component {
   state = {
@@ -14,7 +15,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className={styles.image}>
         <Tracker data={this.state.data}/>
       </div>
     );
--- src/Tracker/Tracker.jsx
+++ src/Tracker/Tracker.jsx
@@ -12,35 +12,35 @@ export function Tracker({
     <div className={styles.center}>
       <h1>
         Infected:{" "}
-        <CountUp
+        <b><CountUp
           start={0}
           end={confirmed.value}
           duration={1}
           separator=","
-          className={styles.red}
-        />
+          className={styles.infected}
+        /></b>
       </h1>
       <h1>
         Recovered:{" "}
-        <CountUp
+        <b><CountUp
           start={0}
           end={recovered.value}
           duration={1}
           separator=","
-          className={styles.green}
-        />
+          className={styles.recovered}
+        /></b>
       </h1>
       <h1>
-        Deaths:{" "}
-        <CountUp
+      Dead:{" "}
+        <b><CountUp
           start={0}
           end={deaths.value}
           duration={1}
           separator=","
-          className={styles.grey}
-        />
+          className={styles.dead}
+        /></b>
       </h1>
-      <h1>{new Date(lastUpdate).toDateString()}</h1>
+      <h1 className={styles.color}>{new Date(lastUpdate).toDateString()}</h1>
     </div>
   );
 }
