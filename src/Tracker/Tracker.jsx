9271fbf0dc0b59f1f7d54ae5b99d76d4c5ff53ea
Shrihan Dadi <dshrihan@gmail.com>
2020-06-28 10:00:25 -0400

    commit

--- src/Tracker/Tracker.jsx
+++ src/Tracker/Tracker.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
 import CountUp from "react-countup";
+import styles from "./Tracker.module.css";
 
 export function Tracker({
   data: { confirmed, recovered, deaths, lastUpdate },
@@ -9,22 +9,38 @@ export function Tracker({
     return "Loading...";
   }
   return (
-    <div>
-      <Typography variant="h3">
+    <div className={styles.center}>
+      <h1>
         Infected:{" "}
-        <CountUp start={0} end={confirmed.value} duration={1} separator="," />
-      </Typography>
-      <Typography variant="h3">
+        <CountUp
+          start={0}
+          end={confirmed.value}
+          duration={1}
+          separator=","
+          className={styles.red}
+        />
+      </h1>
+      <h1>
         Recovered:{" "}
-        <CountUp start={0} end={recovered.value} duration={1} separator="," />
-      </Typography>
-      <Typography variant="h3">
+        <CountUp
+          start={0}
+          end={recovered.value}
+          duration={1}
+          separator=","
+          className={styles.green}
+        />
+      </h1>
+      <h1>
         Deaths:{" "}
-        <CountUp start={0} end={deaths.value} duration={1} separator="," />
-      </Typography>
-      <Typography variant="h3">
-        Date: {new Date(lastUpdate).toDateString()}
-      </Typography>
+        <CountUp
+          start={0}
+          end={deaths.value}
+          duration={1}
+          separator=","
+          className={styles.grey}
+        />
+      </h1>
+      <h1>{new Date(lastUpdate).toDateString()}</h1>
     </div>
   );
 }
