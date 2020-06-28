40c6f5a012f638a0da19205482a921cc0c80c517
Shrihan Dadi <dshrihan@gmail.com>
2020-06-28 11:59:56 -0400

    commit on master

---
+++ build/precache-manifest.142ee859e9eabab90118d18d9d987e5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11d76f64235500ab43450e3adf231d6",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "4245aa99fbeb2b3fc90e",
+    "url": "/covid_tracker/static/css/main.705b9171.chunk.css"
+  },
+  {
+    "revision": "070b2d0f35cf3b97d214",
+    "url": "/covid_tracker/static/js/2.11ee509b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid_tracker/static/js/2.11ee509b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4245aa99fbeb2b3fc90e",
+    "url": "/covid_tracker/static/js/main.cbaeaa31.chunk.js"
+  },
+  {
+    "revision": "4c5145b2abee34e7044b",
+    "url": "/covid_tracker/static/js/runtime-main.fb184295.js"
+  },
+  {
+    "revision": "3a0e9f0ea05117b5167027ec85ceaf2b",
+    "url": "/covid_tracker/static/media/covid.3a0e9f0e.jpg"
+  }
+]);
