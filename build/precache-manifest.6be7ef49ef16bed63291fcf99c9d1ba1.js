5d7b1c6a18e94502d684b467651783744f02bfaf
Shrihan Dadi <dshrihan@gmail.com>
2020-06-28 09:15:55 -0400

    published!

---
+++ build/precache-manifest.6be7ef49ef16bed63291fcf99c9d1ba1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37865c0e592a03e29844d59cf93066d6",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "6fa2df3c6755ef148485",
+    "url": "/covid_tracker/static/js/2.cbba6f9d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covid_tracker/static/js/2.cbba6f9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096a9aea89014d7aa593",
+    "url": "/covid_tracker/static/js/main.74b46a07.chunk.js"
+  },
+  {
+    "revision": "4c5145b2abee34e7044b",
+    "url": "/covid_tracker/static/js/runtime-main.fb184295.js"
+  }
+]);
